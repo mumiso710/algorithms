6f5e6d7313bacb0b24aa058712a3821113ada207
mumiso710 <zkayrodu@gmail.com>
2022-03-14 20:39:56 +0900

    add cpp version

--- part1/week1/main.cpp
+++ part1/week1/main.cpp
@@ -19,16 +19,18 @@ string stringAdd(string x, string y) {
     }
     string ans = string(x.size(), '0');
     y = paddingZeros(y, x.size() - y.size());
-    for (int i = x.size() - 1; i > 0; i--) {
-        bool carry;
+    bool carry = false;
+    for (int i = x.size() - 1; i >= 0; i--) {
        int sum = x[i] - '0' + y[i] - '0';
+       if (carry) sum += 1;
        if (sum >= 10) {
            sum -= 10;
            carry = true;
-       } 
+       } else {
+           carry = false;
+       }
        ans[i] = sum + '0';
-       if (carry && i > 0) ans[i-1] + 1;
-       if (carry && i == 0) ans = '1' + ans;
+       if (carry && i == 0) ans = "1" + ans;
     }
     return ans;
 }
@@ -40,27 +42,45 @@ string stringSub(string x, string y) {
     }
     string ans = string(x.size(), '0');
     y = paddingZeros(y, x.size() - y.size());
-    bool carry;
-    for (int i = x.size() - 1; i > 0; i--) {
+    bool carry = false;
+    for (int i = x.size() - 1; i >= 0; i--) {
        int sub = x[i] - '0' - (y[i] - '0');
+       if (carry) sub -= 1;
        if (sub < 0) {
            sub += 10;
            carry = true;
-       } 
-       if (carry && i > 0) sub - 1;
+       } else {
+           carry = false;
+       }
        ans[i] = sub + '0';
-       if (carry && i == 0) ans = '-' + ans;
     }
     return ans;
 
 }
 
+string trimZeros(string s) {
+    for (int i=0; i<s.size(); i++) {
+        if (s[i] == '0') {
+            continue;
+        } else {
+            return s.substr(i);
+        }
+    }
+}
+
 string karatsuba(string x, string y) {
-    int n = x.size();
+    if (x == to_string(0)) return to_string(0);
+    int n = max(x.size(), y.size());
+    x = paddingZeros(x, n - x.size());
+    y = paddingZeros(y, n - y.size());
 
-    if (n == 1) return string(1, (x[0] - '0' * y[0] - '0') + '0');
-
-    else {
+    if (n == 1) return to_string((x[0] - '0') * (y[0] - '0'));
+    
+    if (n % 2 != 0) {
+        x = paddingZeros(x, 1);
+        y = paddingZeros(y, 1);
+        n++;
+    }
        string a, b, c, d;
        a = x.substr(0, n / 2);
        b = x.substr(n / 2);
@@ -68,8 +88,8 @@ string karatsuba(string x, string y) {
        d = y.substr(n / 2);
 
        string p, q;
-       p = a + b;
-       q = c + d;
+       p = stringAdd(a, b);
+       q = stringAdd(c, d);
 
        string ac, bd, pq, adbc;
        ac = karatsuba(a, c);
@@ -77,13 +97,14 @@ string karatsuba(string x, string y) {
        pq = karatsuba(p, q);
        adbc = stringSub(stringSub(pq, ac), bd);
 
-       return stringAdd(stringAdd(stringPow10(ac, n), stringPow10(adbc, n / 2)), bd);
-    }
+       return trimZeros(stringAdd(stringAdd(stringPow10(ac, n), stringPow10(adbc, n / 2)), bd));
 }
 
 int main() {
     string a = "3141592653589793238462643383279502884197169399375105820974944592";
     string b = "2718281828459045235360287471352662497757247093699959574966967627";
-
+    
+    //  cin >> a >> b;
+    
     cout << karatsuba(a, b) << endl;
 }
